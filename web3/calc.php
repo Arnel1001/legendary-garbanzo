64202bc62265d04266744c43a55e6a6e7c666364
Marc Deloso <202080169@psu.palawan.edu.ph>
2023-03-02 23:03:53 +0800

    web3 done

--- web3/calc.php
+++ web3/calc.php
@@ -2,17 +2,33 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+        $value_1 = $_POST['val1'];
+        $value_2 = $_POST['val2'];
+        $operator = $_POST['calc'];
         # test if input is_numeric
-        if(1){
+        if(is_numeric($value_1) && is_numeric($value_2)){
+            
             # test if calc is not null
-            if(1){
+            if(isset($operator)){
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($operator){
+                    case "add":
+                        $result = $value_1 + $value_2;
+                        break;
+                    case "sub":
+                        $result = $value_1 - $value_2;
+                        break;
+                    case "mul":
+                        $result = $value_1 * $value_2;
+                        break;
+                    case "div":
+                        $result = $value_1 / $value_2;
+                        break;
+                }
                 }
                 echo "Calculation result: $result";
             }
-        }
+
         else{ echo "Invalid entry - please retry"; }
     ?>
 </body>
