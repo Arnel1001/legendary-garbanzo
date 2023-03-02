2075b814c9a509cc1e3712011cd4bd4edcbd2dad
Marc Deloso <202080169@psu.palawan.edu.ph>
2023-03-02 23:09:27 +0800

    web5 done

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,23 @@
 <?php
+session_start();
+if(isset($_SESSION['view'])){
+    $_SESSION['view'] = $_SESSION['view'] + 1;
+}else{
+    $_SESSION['view'] = 1;
+}
 
+if($_SESSION['view'] == 10){
+    header("Location: congratulations.php");
+}
 ?>
 
 <h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
+<p>You have visited this page <?php $num_visits = $_SESSION['view'];
+echo $num_visits?> times</p>
 <p><a href="?reset=true">Reset</a></p>
+
+<?php 
+if($_SESSION['view'] == 5){
+    echo "Thank you for visiting the page a lot.";
+}
+?>
